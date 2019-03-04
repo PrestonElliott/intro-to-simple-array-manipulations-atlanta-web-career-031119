ca33bdb2433fbe3ed117fd86939f66a53a06b851
Preston Elliott <preston.elliott24@gmail.com>
2019-03-04 16:06:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,7 +27,10 @@ def using_concat(array_1, array_2)
   array_1.concat(array_2)
 end
 
-# COME BACK TO THIS PROBLEM
 def using_insert(array, element)
   array.insert(4, element)
+end
+
+def using_uniq(array)
+  array.uniq 
 end
