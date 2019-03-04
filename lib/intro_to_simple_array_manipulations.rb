981dfd72c097f1a14d39f3b306c5f09d4cad6256
Preston Elliott <preston.elliott24@gmail.com>
2019-03-04 16:04:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,6 +27,7 @@ def using_concat(array_1, array_2)
   array_1.concat(array_2)
 end
 
-def using_insert(array, element)
-  array.insert(3, element)
-end
+# COME BACK TO THIS PROBLEM
+# def using_insert(array, element)
+#   array.insert(3, element)
+# end
